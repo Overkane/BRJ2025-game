7d31c1eac4fd30d3838ff48dd9dd74a292b918ef
Overkane <overkane@mail.ru>
2025-01-14 18:45:31 +0500

    Main Menu WIP

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -1,8 +1,49 @@
-extends CanvasLayer
+extends Control
 
-@export var boss_arena: PackedScene 
+@onready var _menu = $Menu
+@onready var _credits = $Credits
+@onready var _options_menu = $OptionsMenu
+@onready var _quit_button = $Menu/QuitButton
+
+const BOSS_ARENA_SCENE := preload("res://scenes/boss_arena/boss_arena.tscn")
+
+enum MenuOptions { MAIN_MENU, OPTIONS, CREDITS }
+
+
+func _ready() -> void:
+	if OS.has_feature("web"):
+		_quit_button.hide()
+		
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_packed(boss_arena)
+	get_tree().change_scene_to_packed(BOSS_ARENA_SCENE)
+
+func _on_options_button_pressed() -> void:
+	switch_menu_option(MenuOptions.OPTIONS)
+
+func _on_options_menu_return_to_main_menu() -> void:
+	switch_menu_option(MenuOptions.MAIN_MENU)
+
+func _on_credits_button_pressed() -> void:
+	switch_menu_option(MenuOptions.CREDITS)
+
+func _on_credits_return_to_main_menu() -> void:
+	switch_menu_option(MenuOptions.MAIN_MENU)
+
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func switch_menu_option(menu_option: MenuOptions):
+	if menu_option == MenuOptions.MAIN_MENU:
+		_menu.show()
+		_options_menu.hide()
+		_credits.hide()
+	elif  menu_option == MenuOptions.OPTIONS:
+		_menu.hide()
+		_options_menu.show()
+		_credits.hide()
+	elif menu_option == MenuOptions.CREDITS:
+		_menu.hide()
+		_options_menu.hide()
+		_credits.show()
