4d766d27ad5dba3aa0d8603d0cd01c267091eab3
Overkane <overkane@mail.ru>
2025-01-14 22:17:52 +0500

    Finished audio settings

--- scenes/main_menu/options/options_menu.gd
+++ scenes/main_menu/options/options_menu.gd
@@ -2,6 +2,27 @@ extends Control
 
 signal return_to_main_menu
 
+@onready var _master_slider = $MarginContainer/VBoxContainer/SplitContainer/AudioSettingsContainer/MarginContainer/MasterSlider
+@onready var _music_slider = $MarginContainer/VBoxContainer/SplitContainer/AudioSettingsContainer/MarginContainer2/MusicSlider
+@onready var _sfx_slider = $MarginContainer/VBoxContainer/SplitContainer/AudioSettingsContainer/MarginContainer3/SFXSlider
+
+enum SoundBuses { MASTER, MUSIC, SFX }
+
+
+func _ready():
+	_master_slider.set_value_no_signal(db_to_linear(AudioServer.get_bus_volume_db(SoundBuses.MASTER)))
+	_music_slider.set_value_no_signal(db_to_linear(AudioServer.get_bus_volume_db(SoundBuses.MUSIC)))
+	_sfx_slider.set_value_no_signal(db_to_linear(AudioServer.get_bus_volume_db(SoundBuses.SFX)))
+
+
+func _on_master_slider_value_changed(value:float) -> void:
+	AudioServer.set_bus_volume_db(SoundBuses.MASTER, linear_to_db(value))
+
+func _on_music_slider_value_changed(value:float) -> void:
+	AudioServer.set_bus_volume_db(SoundBuses.MUSIC, linear_to_db(value))
+
+func _on_sfx_slider_value_changed(value:float) -> void:
+	AudioServer.set_bus_volume_db(SoundBuses.SFX, linear_to_db(value))
 
 func _on_back_button_pressed() -> void:
 	return_to_main_menu.emit()
